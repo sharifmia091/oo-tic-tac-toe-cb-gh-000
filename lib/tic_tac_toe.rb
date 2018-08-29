d2d220be17b366efc2d129b4850dd8338f88463c
md mia <sharifmia091@gmail.com>
2018-08-29 05:18:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,4 +102,30 @@ class TicTacToe
   end
 
 
+  def winner 
+    index = []
+    index = won?
+    if index == false
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
+      end
+    end
+  end
+  
+  def play
+    until over? == true
+      turn(board)
+    end
+  
+    if won?
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
