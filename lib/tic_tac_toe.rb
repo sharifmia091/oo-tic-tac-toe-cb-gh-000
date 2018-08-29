605e093159f7f731efc5cc962056852d44901ea2
md mia <sharifmia091@gmail.com>
2018-08-29 05:35:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,8 +22,9 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+  
   def input_to_index(user_input)
-    user_input.to_i -1
+    user_input.to_i - 1
   end
 
   def position_taken?(index)
